84fe29841207280096ce1457ded0fa65420dc518
David Shen <me@pantherman594.com>
2016-07-20 11:52:45 -0400

    Fix blazin cache download

--- downloadBlazin.sh
+++ downloadBlazin.sh
@@ -2,4 +2,4 @@
 cd /var/www/d-shen/cliqbit
 mkdir -p blazinCache
 cd blazinCache
-wget https://api.cliqbitapp.com/CustomAPI_br8tradevusteswe_v1.14/rest/feed/blazinBits/1/0/75 -o api.json
+wget https://api.cliqbitapp.com/CustomAPI_br8tradevusteswe_v1.14/rest/feed/blazinBits/1/0/75 -O api.json
