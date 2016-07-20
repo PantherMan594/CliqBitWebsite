fda2cb27ef440c7707c3f3116b184f0cc7ebd7d6
David Shen <me@pantherman594.com>
2016-07-20 12:39:42 -0400

    Add custom 404 page

---
+++ 404.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head lang="en">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="theme-color" content="#f28530">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="HandheldFriendly" content="true">
+	
+	<link href="./favicon.png" rel="shortcut icon">
+	<title>404 Page not found | CliqBit</title>
+</head>
+<body>
+	<header></header>
+	<nav>
+		<ul id="nav-pre">
+			<li id="tagline">Share your not-so-serious side.</li>
+			<li ><a id="download" href="https://itunes.apple.com/us/app/cliqbit/id1029040166?ls=1&mt=8">Download Now!</a></li>
+		</ul>
+		<ul id="nav">
+			<li><a id="logo" href="./"><span>filler</span></a></li>
+			<li><a href="./">Home</a></li>
+			<li><a href="blazin">Blazin' Bits</a></li>
+			<li><a href="about">About Us</a></li>
+			<li><a href="contact">Contact</a></li>
+			<li><a href="https://twitter.com/CliqBit"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+		</ul>
+	</nav>
+	<div id="content">
+		<h3>Joke's on you!</h3>
+		<p>Uh oh, you came to the wrong place. This is not CliqBit! In fact... this page doesn't exist! You should go back to our <a href="./">home page</a> and continue from there.</p>
+	</div>
+</body>
+<link type="text/css" rel="stylesheet" href="assets/css/style.css">
+<link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+</html>
