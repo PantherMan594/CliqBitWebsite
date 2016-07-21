b72ae3462d1c458d97ded696a7c16c49d16c8aad
David Shen <me@pantherman594.com>
2016-07-21 10:08:52 -0400

    Create contact page, update features and about pages

--- about.php
+++ about.php
@@ -2,28 +2,27 @@
 <html>
 <head lang="en">
 	<meta charset="UTF-8">
-	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="theme-color" content="#f28530">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="HandheldFriendly" content="true">
 	
-	<link href="favicon.png" rel="shortcut icon">
-	<title>About Us - CliqBit</title>
-	<meta name="description" content="CliqBit | Our app comes packed with features, ranging from disappearing posts to memes to a built-in editor!">
-	
-	<script type="text/javascript" src="assets/js/jquery-3.0.0.min.js"></script>
-	
-	<link type="text/css" rel="stylesheet" href="assets/css/style.css">
+	<link href="./favicon.png" rel="shortcut icon">
+	<title>About Us | CliqBit</title>
+	<meta name="description" content="CliqBit | Get to know the founders of CliqBit!">
 </head>
 <body>
-	<header>
-	</header>
+	<a href="#" class="back-to-top"><i class="fa fa-chevron-up fa-3x" aria-hidden="true"></i></a>
+	<header></header>
 	<nav>
-		<ul>
+		<ul id="nav-pre">
 			<li id="tagline">Share your not-so-serious side.</li>
-			<li><a id="download" href="https://itunes.apple.com/us/app/cliqbit/id1029040166?ls=1&mt=8">Download Now!</a></li>
-			<br />
+			<li ><a id="download" href="https://itunes.apple.com/us/app/cliqbit/id1029040166?ls=1&mt=8">Download Now!</a></li>
+		</ul>
+		<ul id="nav">
 			<li><a id="logo" href="./"><span>filler</span></a></li>
 			<li><a href="./">Home</a></li>
+			<li><a href="blazin">Blazin' Bits</a></li>
 			<li><a id="nav-active" href="about">About Us</a></li>
 			<li><a href="contact">Contact</a></li>
 			<li><a href="https://twitter.com/CliqBit"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
@@ -42,4 +41,40 @@
 		</div>
 	</div>
 </body>
+<link type="text/css" rel="stylesheet" href="assets/css/style.css">
+<link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+<a title="Web Statistics" href="http://clicky.com/100974674"><img alt="Web Statistics" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+<script type="text/javascript">
+var clicky_site_ids = clicky_site_ids || [];
+clicky_site_ids.push(100974674);
+(function() {
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//static.getclicky.com/js';
+  ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+})();
+</script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100974674ns.gif" /></p></noscript>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script type="text/javascript">
+	var amountScrolled = 300;
+
+	$(window).scroll(function(){
+		var scrollTop = $(window).scrollTop();
+		
+		if (scrollTop > amountScrolled ) {
+			$('a.back-to-top').fadeIn('slow');
+		} else {
+			$('a.back-to-top').fadeOut('slow');
+		}
+	});
+	
+	$('a.back-to-top').click(function() {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 700);
+		return false;
+	});
+</script>
 </html>
--- contact.php
+++ contact.php
@@ -8,8 +8,8 @@
 	<meta name="HandheldFriendly" content="true">
 	
 	<link href="./favicon.png" rel="shortcut icon">
-	<title>Features | CliqBit</title>
-	<meta name="description" content="CliqBit | Our app comes packed with features, ranging from disappearing posts to memes to a built-in editor!">
+	<title>Contact | CliqBit</title>
+	<meta name="description" content="CliqBit | Contact us for questions, comments, requests, or reports!">
 </head>
 <body>
 	<a href="#" class="back-to-top"><i class="fa fa-chevron-up fa-3x" aria-hidden="true"></i></a>
@@ -24,40 +24,12 @@
 			<li><a href="./">Home</a></li>
 			<li><a href="blazin">Blazin' Bits</a></li>
 			<li><a href="about">About Us</a></li>
-			<li><a href="contact">Contact</a></li>
+			<li><a id="nav-active" href="contact">Contact</a></li>
 			<li><a href="https://twitter.com/CliqBit"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
 		</ul>
 	</nav>
 	<div id="content" class="centered">
-		<h1>Features</h1>
-		<hr>
-		<h2 id="laugh">We Make You laugh</h2>
-		<p>No description needed. But we'll give you one anyway. We're the funny-focused social media app. Follow your friends, share your funny moments, and get ready to roar.</p>
-		<img src="assets/img/laugh.png" alt="Laughing CliqBit face" />
-		<hr>
-		<h2 id="disappearing-posts">Disappearing Posts</h2>
-		<p>Don’t want something sticking around forever? We’ve got you—have it disappear after 5 seconds or 48 hours. Or make it a permanent post if you're brave.</p>
-		<img src="assets/img/disappearing.png" alt="Example of what a disappearing post would look like" />
-		<hr>
-		<h2 id="memes-stopmotion-more">Memes, Stop-Motion, and more</h2>
-		<p>Humor comes in many forms. Videos, photos, text posts—it’s all here. Mess around with memes, screenshots, and stop-motion videos to really get everyone roaring.</p>
-		<img src="assets/img/memes.png" alt="An example meme you can create with our app" />
-		<hr>
-		<h2 id="cliqs">Cliqs</h2>
-		<p>We all have inside jokes. Don't want something going on your main newsfeed? Swipe left to share within a cliq! Invite your friends to these private feeds that have the same taste (or lack thereof) and have a blast!</p>
-		<img src="assets/img/cliqs.png" alt="What a cliq page looks like" />
-		<hr>
-		<h2 id="blazin-bits">Blazin' Bits</h2>
-		<p>Wanna know what’s burning up on CliqBit? Click the fire icon! The hottest posts are here on a separate feed.</p>
-		<img src="assets/img/blazin.png" alt="Blazin' Bits icon" />
-		<hr>
-		<h2 id="secret-cheers">Secret Cheers Count</h2>
-		<p>Cheers are our version of likes--without all the competition. Only you can see who’s cheered your posts. Less data, less drama.</p>
-		<img src="assets/img/cheers.png" alt="CliqBit cheers button" />
-		<hr>
-		<h2 id="profile">Profile</h2>
-		<p>A collection of your non-dissapearing posts. Keep some stuff around. But, don't worry — you can always go back and delete them.</p>
-		<img src="assets/img/profile.png" alt="Example profile page" />
+		<h1>Coming Soon!</h1>
 	</div>
 </body>
 <link type="text/css" rel="stylesheet" href="assets/css/style.css">
