8810afbd856d1a9825ca9a947eeff7ba009b5582
David Shen <me@pantherman594.com>
2016-07-25 13:29:50 -0400

    Have style override font-awesome

--- contact.php
+++ contact.php
@@ -32,8 +32,8 @@
 		<h1>Coming Soon!</h1>
 	</div>
 </body>
-<link type="text/css" rel="stylesheet" href="assets/css/style.css">
 <link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+<link type="text/css" rel="stylesheet" href="assets/css/style.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script type="text/javascript">
 	var amountScrolled = 300;
--- features.php
+++ features.php
@@ -60,8 +60,8 @@
 		<img src="assets/img/profile.png" alt="Example profile page" />
 	</div>
 </body>
-<link type="text/css" rel="stylesheet" href="assets/css/style.css">
 <link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+<link type="text/css" rel="stylesheet" href="assets/css/style.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script type="text/javascript">
 	var amountScrolled = 300;
