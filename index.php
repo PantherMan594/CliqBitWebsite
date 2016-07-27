c366e63c6b13d0ba8338abd74beac09bd5d91f83
David Shen <me@pantherman594.com>
2016-07-27 14:03:27 -0400

    Add global.js link

--- index.php
+++ index.php
@@ -81,6 +81,7 @@
 <link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
 <link type="text/css" rel="stylesheet" href="assets/css/style.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="assets/js/global.js"></script>
 <script type="text/javascript">
 	var elementPosition = $('#demo').offset();
 	var imgOffset = elementPosition.top - $('.funny').offset().top;
