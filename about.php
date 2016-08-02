f0402389ee1fcc5b963d2c1dd02f3fd44c52c88c
David Shen <me@pantherman594.com>
2016-08-02 14:48:34 -0400

    Add press articles to about me, add serious about me info

--- about.php
+++ about.php
@@ -35,7 +35,8 @@
 		<p class="indent">Did we just get bored during our first year at college? Definitely not. We admittedly stumbled upon this idea while procrastinating studying for finals, but we couldn't give it up.</p>
 		<p class="centered">We've been sleep deprived ever since.</p>
 		<br>
-		<img class="centeredImg" src="assets/img/wellesley.png" />
+		<img class="centeredImg rounded-50" src="assets/img/wellesley.png" alt="Wellesley College sleep when dead" />
+		<br>
 		<div class="indent" >
 			<p>In the winter of 2014, Hannah and Olivia stood at the bottom of a campus hill, doubled over in laughter they recounted the less than graceful moment that had just happened. On her way back from the dining hall, Hannah's feet came straight out from under her, her books flying out of her arms, and she proceeded to slip all the way down an icy hill. The fall was almost something out of a movie.</p>
 			<p>To Hannah's surprise, the other witness was one of the only guys on their all-women’s campus — the handsome campus police officer. Of course. Surprise aside, they had one big regret: no one caught it on camera. And with status updates being outdated, there wasn't a place to share the moment with friends.</p>
@@ -48,34 +49,88 @@
 		<p>In the end, there wasn't a platform designed to allow you to be yourself in all your personal, funny, candid glory. Until now.</p>
 		<br>
 		<p class="centered">CliqBit is the humorous social media platform.</p>
-		<br><br>
+		<br>
+		<h1 class="centered">The Talk</h1>
+		<hr>
+		<div id="press">
+			<div>
+				<a href="http://dailyfreepress.com/2016/02/04/cliqbit-startup-app-humerous/"><h2>The Daily Free Press:</h2><h3>CliqBit Startup Allows Users to Share Humorous Snippets</h3></a>
+				<p>For CliqBit co-founders, Olivia Joslin and Hannah Wei, the idea emerged from an embarrassing, but also humorous, accident.</p>
+				<p>“Hannah and I were on our icy campus in the middle of last winter and she slipped and fell down in front of the only cute guy on our women’s college campus … I was trying to open my Snapchat to get a photo, but the app crashed and the moment was lost,” Joslin said... <a href="http://dailyfreepress.com/2016/02/04/cliqbit-startup-app-humerous/">Read more.</a></p>
+			</div>
+			<div>
+				<a href="http://bostinno.streetwise.co/2016/02/03/snapchat-facebook-alternative-cliqbit-social-media-app/"><h2>BostInno:</h2><h3>The Wellesley-Born CliqBit Is Your Social Media Haven for Silly Moments</h3></a>
+				<p>For young women like Olivia Joslin and Hannah Wei, posting to social media platforms like Instagram can feel like a job, where perfection and vanity reign supreme and a whole etiquette has emerged on how to compliment your peers’ agonizingly choreographed selfies. Don’t believe me? Just listen to this This American Life episode from November. Or check out this screed from Australian teenager Essena O’Neil, who accrued 612,000 followers in Instagram and later decried social media as fake, revealing the sad truth behind her “perfect” photos... <a href="http://bostinno.streetwise.co/2016/02/03/snapchat-facebook-alternative-cliqbit-social-media-app/">Read more.</a></p>
+			</div>
+			<div>
+				<a href="http://www.theplainsman.com/article/2016/02/cliqbit-app-allows-room-for-humor-on-social-media"><h2>The Auburn Plainsman:</h2><h3>CliqBit App Allows Room for Humor on Social Media</h3></a>
+				<p>As you’re scrolling through your newsfeed on Facebook, you notice something.</p>
+				<p>Either everyone is suddenly a model, just purchased a professional camera or just really wants more likes on that profile picture.</p>
+				<p>A likely reason, though, is that employers are studying Facebook profiles almost as much as résumés.</p>
+				<p>Facebook, then, is not the ideal place to be writing inside jokes with your friends or sharing goofy, unprofessional articles... <a href="http://www.theplainsman.com/article/2016/02/cliqbit-app-allows-room-for-humor-on-social-media">Read more.</a></p>
+			</div>
+			<div>
+				<a href="http://bcheights.com/metro/2016/new-app-cliqbit-seeks-to-create-a-positive-social-media-atmosphere/"><h2>The Heights:</h2><h3>CliqBit: A New App That Creates a Social Media Haven, Founders Say</h3></a>
+				<p>Frustrated with the lack of an ideal platform to share funny moments, Olivia Joslin and Hannah Wei, Wellesley ’18, created CliqBit. CliqBit is similar to Snapchat, allowing users to share their photos, videos, and posts with a specific group of friends for a limited timeframe that can range from only 5 seconds to as long as 48 hours... <a href="http://bcheights.com/metro/2016/new-app-cliqbit-seeks-to-create-a-positive-social-media-atmosphere/">Read more.</a></p>
+			</div>
+			<div>
+				<a href="http://thewellesleynews.com/2016/02/03/wellesley-sophomores-create-social-media-platform-to-capture-and-share-lifes-bloopers/"><h2>The Wellesley News:</h2><h3>Wellesley Sophmores Create Social Media Platform to Capture and Share Life's Bloopers</h3></a>
+				<p>During Wellesley’s finals period in fall 2014, Hannah Wei ’18 and Olivia Joslin ’18 were in the initial stages of creating a brand: CliqBit. A social media application designed for iPhones, CliqBit serves as a new platform for users to share, as Wei describes, “life’s bloopers” in the form of disappearing posts, memes, videos and more.... <a href="http://thewellesleynews.com/2016/02/03/wellesley-sophomores-create-social-media-platform-to-capture-and-share-lifes-bloopers/">Read more.</a></p>
+			</div>
+			<div>
+				<a href="http://bostinno.streetwise.co/2016/02/12/new-mobile-apps-in-2016-made-by-boston-consumer-startups/"><h2>BostInno:</h2><h3>Here Are 5 Boston Mobile Startups to Watch</h3></a>
+				<p>CliqBit is a new kind of social media app that recently launched, and it combines elements of Facebook, Instagram and SnapChat. Most importantly, however, the app aims to shed the vanity and perfection that has come to dominate these social networks for many young people, especially women. Instead, the app wants to be a haven for the silly moments of your life. Beyond having some similar functionality to Facebook and other social networks, the app also lets you create your own memes and stop-motion videos... <a href="http://bostinno.streetwise.co/2016/02/12/new-mobile-apps-in-2016-made-by-boston-consumer-startups/">Read more.</a></p>
+			</div>
+		</div>
+		<h3 class="centered">Please contact <a href="mailto:ojoslin@cliqbitapp.com">ojoslin@cliqbitapp.com</a> for any press inquiries.</h3>
+		<br>
 		<h1 class="centered">The Founders</h1>
 		<hr>
+		<img class="centeredImg rounded-50" src="assets/img/hannahOlivia.png" alt="Picture of Hannah and Olivia" />
 		<div id="col_wrap">
 			<div id="left_col">
-				<img class="centeredImg" src="assets/img/olivia.png" />
-				<h2>Olivia</h2>
-				<p>I use a lot of social media platforms, but I don’t always enjoy the time I spend on them. Ready to ditch the news and drama of other apps, Hannah and I started CliqBit to switch things up. Growing up, I was always known as the girl who giggled. I think I could find humor in just about anything, and I think CliqBit encourages us all to do the same. On average, everyone has 4-5 good laughs a day and it's time there was a place to share them. I’m looking forward to sharing my funny moments just much as I am to seeing yours! Cheers!</p>
-				<div class="funFacts">
-					<h3>Fun(ny) facts:</h3>
-					<ul>
-						<li>I’m terrified of high ceilings.</li>
-						<li>I’ve always wanted to be a racecar driver. I've also always wanted to live past the age of 20, so I'm glad I didn't choose this career path. I'm defintely not the world's best driver.</li>
-						<li>I think I eat more dessert than regular food, which explains why I thought a donut would make an excellent Halloween costume. I dragged Hannah into my plan as well. You can't have just one donut.</li>
-					</ul>
+				<img class="centeredImg oval" src="assets/img/olivia.png" alt="Serious picture of Olivia" />
+				<h2>Olivia Joslin</h2>
+				<h2>Co-Founder</h2>
+				<p>Olivia Joslin is the co-founder and CEO of CliqBit, as well as a student at Wellesley College in Wellesley, Massachusetts. She’s originally from Hilton Head Island, South Carolina where she discovered a love for the both environmental studies and computer science.</p>
+				<p>Olivia is a decorated researcher; she won awards at the Intel International Science Fair and holds research titles at both the Department of Natural Resources and the National Oceanic & Atmospheric Administration.</p>
+				<p>At CliqBit, Olivia enjoys combining her passion for tech and social media. She leads all business operations and is also responsible for directing the app’s user experience and directs the company’s vision. Olivia is confident in the power of a daily laugh.</p>
+				<hr>
+				<div class="hide-small">
+					<img class="centeredImg rounded-100" src="assets/img/olivia2.png" alt="Silly picture of Olivia" />
+					<p>I use a lot of social media platforms, but I don’t always enjoy the time I spend on them. Ready to ditch the news and drama of other apps, Hannah and I started CliqBit to switch things up.</p>
+					<p>Growing up, I was always known as the girl who giggled. I think I could find humor in just about anything, and I think CliqBit encourages us all to do the same. On average, everyone has 4-5 good laughs a day and it's time there was a place to share them. I’m looking forward to sharing my funny moments just much as I am to seeing yours! Cheers!</p>
+					<div class="funFacts">
+						<h3>Fun(ny) facts:</h3>
+						<ul>
+							<li>I’m terrified of high ceilings.</li>
+							<li>I’ve always wanted to be a racecar driver. I've also always wanted to live past the age of 20, so I'm glad I didn't choose this career path. I'm defintely not the world's best driver.</li>
+							<li>I think I eat more dessert than regular food, which explains why I thought a donut would make an excellent Halloween costume. I dragged Hannah into my plan as well. You can't have just one donut.</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 			<div id="right_col">
-				<img class="centeredImg" src="assets/img/hannah.png" />
-				<h2>Hannah</h2>
-				<p>I’ve always had a lot of silly moments. I’d be that one friend who flipped over her bike on a biking trip, and the one who scores the 'winning shot'—but in the wrong basket. I love making people laugh with my daily life, whether the people are my closest friends, new acquaintances, or even the special needs children I work with. In high school, I actually started an improv group to encourage people to laugh more at their everyday funny moments. CliqBit came about because Olivia and I really wanted to have a more suitable platform to comfortably share these hysterical moments with our friends and to avoid missing out on everyone’s life bloopers (#endFOMO)! If you can’t tell already, we love CliqBit, and we hope you will soon, too!</p>
-				<div class="funFacts">
-					<h3>Fun(ny) facts:</h3>
-					<ul>
-						<li>I have a long list of irrational fears, including high-pressure cookers and potential-snakes-in-the-toilet.</li>
-						<li>I'm fluent in 4 languages, which essentially means that it takes me a while to switch language modes when texting.</li>
-						<li>I have a detached retina in my right eye. Often I can only detect light like I’m an earthworm with an eyespot. So life is pretty exciting for me.</li>
-					</ul>
+				<img class="centeredImg oval" src="assets/img/hannah.png" alt="Serious picture of Hannah" />
+				<h2>Hannah Wei</h2>
+				<h2>Co-Founder</h2>
+				<p>Hannah Wei is the co-founder of CliqBIt and a student at Wellesley College in Wellesley, Massachusetts.  Originally from Taiwan, she was encouraged to pursue a degree in STEM as early as third grade and published her first research paper in high school.</p>
+				<p>In addition to studying Cinema & Media Studies with a pre-medical track, Hannah is pursuing a degree in Computer Science. Her studies have supported her work with the back-end infrastructure of the app, and she’s looking forward to tackling the unique challenges of being a woman in technology.</p>
+				<p>Hannah is a certified lab researcher and a Red Cross lifeguard, and is fluent in four languages. She is passionate about helping others.</p>
+				<hr>
+				<div class="hide-small">
+					<img class="centeredImg rounded-100" src="assets/img/hannah2.png" alt="Silly picture of Hannah" />
+					<p>I’ve always had a lot of silly moments. I’d be that one friend who flipped over her bike on a biking trip, and the one who scores the 'winning shot'—but in the wrong basket. I love making people laugh with my daily life, whether the people are my closest friends, new acquaintances, or even the special needs children I work with.</p>
+					<p>In high school, I actually started an improv group to encourage people to laugh more at their everyday funny moments. CliqBit came about because Olivia and I really wanted to have a more suitable platform to comfortably share these hysterical moments with our friends and to avoid missing out on everyone’s life bloopers (#endFOMO)!</p>
+					<p>If you can’t tell already, we love CliqBit, and we hope you will soon, too!</p>
+					<div class="funFacts">
+						<h3>Fun(ny) facts:</h3>
+						<ul>
+							<li>I have a long list of irrational fears, including high-pressure cookers and potential-snakes-in-the-toilet.</li>
+							<li>I'm fluent in 4 languages, which essentially means that it takes me a while to switch language modes when texting.</li>
+							<li>I have a detached retina in my right eye. Often I can only detect light like I’m an earthworm with an eyespot. So life is pretty exciting for me.</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -89,23 +144,27 @@
 <script src="assets/js/global.js"></script>
 <script type="text/javascript">
 	
-	$('#col_wrap').mouseleave(function() {
-		$('#col_wrap > div').animate({'width': '50%'}, 'fast');
-		$('.funFacts').fadeIn('fast');
-	});
-	$('#left_col').hover(function() {
-		$('#right_col .funFacts').fadeOut('fast');
-		$('#right_col').animate({'width': '25%'}, 'fast');
-		$(this).animate({'width': '75%'}, 'fast');
-	}, function() {
-		$('#right_col .funFacts').fadeIn('fast');
-	});
-	$('#right_col').hover(function() {
-		$('#left_col .funFacts').fadeOut('fast');
-		$('#left_col').animate({'width': '25%'}, 'fast');
-		$(this).animate({'width': '75%'}, 'fast');
-	}, function() {
-		$('#left_col .funFacts').fadeIn('fast');
+	$(document).ready(function() {
+		if ($(window).width() > 765) {
+			$('#col_wrap').mouseleave(function() {
+				$('#col_wrap > div').animate({'width': '50%'}, 'fast');
+				$('.hide-small').fadeIn('fast');
+			});
+			$('#left_col').hover(function() {
+				$('#right_col .hide-small').fadeOut('fast');
+				$('#right_col').animate({'width': '25%'}, 'fast');
+				$(this).animate({'width': '75%'}, 'fast');
+			}, function() {
+				$('#right_col .hide-small').fadeIn('fast');
+			});
+			$('#right_col').hover(function() {
+				$('#left_col .hide-small').fadeOut('fast');
+				$('#left_col').animate({'width': '25%'}, 'fast');
+				$(this).animate({'width': '75%'}, 'fast');
+			}, function() {
+				$('#left_col .hide-small').fadeIn('fast');
+			});
+		}
 	});
 </script>
 </html>
